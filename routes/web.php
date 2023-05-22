f392c74fac4c53dd448218fa95f6dee28bfb4df3
mhdtaufikrm <mtaufikramadhan27@gmail.com>
2023-05-22 22:35:19 +0700

    22-Mei-2023

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\IndexController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/', [IndexController::class, 'index']);
+
+/* Route::get('/', function () {
     return view('index');
-});
+}); */
